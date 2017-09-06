2e3d76ec3bf65908ca590e7def769b12f16ef95b
Dennis Durling <djdtahoe@gmail.com>
2017-09-06 12:50:56 -0700

    updated documentation for added CancelServer

--- servers.go
+++ servers.go
@@ -6,6 +6,7 @@ import (
 	"strconv"
 )
 
+//Server struct defines what a VPS looks like
 type Server struct {
 	Name         string `json:"fqdn"`
 	ID           int    `json:"mbpkgid,string"`
@@ -20,16 +21,19 @@ type Server struct {
 	PowerStatus  string `json:"state"`
 }
 
+//ServerOptions struct defines some extra options including SSH Auth
 type ServerOptions struct {
 	SSHKeyID    int
 	Password    string
 	CloudConfig string
 }
 
+//JobID struct holds the current Job Id for what's being processed
 type JobID struct {
 	ID int `json:"id,string"`
 }
 
+//GetServers external method on Client to list your instances
 func (c *Client) GetServers() ([]Server, error) {
 
 	var serverList []Server
@@ -41,6 +45,7 @@ func (c *Client) GetServers() ([]Server, error) {
 	return serverList, nil
 }
 
+//GetServer external method on Client to get an instance
 func (c *Client) GetServer(id int) (server Server, err error) {
 	if err := c.get("/cloud/server/"+strconv.Itoa(id), &server); err != nil {
 		return Server{}, err
@@ -48,6 +53,7 @@ func (c *Client) GetServer(id int) (server Server, err error) {
 	return server, nil
 }
 
+//StartServer external method on Client to boot up an instance
 func (c *Client) StartServer(id int) error {
 
 	if err := c.post("/cloud/server/start/"+strconv.Itoa(id), nil, nil); err != nil {
@@ -57,6 +63,7 @@ func (c *Client) StartServer(id int) error {
 	return nil
 }
 
+//StopServer external method on Client to shut down an instance
 func (c *Client) StopServer(id int) error {
 
 	if err := c.post("/cloud/server/shutdown/"+strconv.Itoa(id), nil, nil); err != nil {
@@ -66,6 +73,7 @@ func (c *Client) StopServer(id int) error {
 	return nil
 }
 
+//RebootServer external method on Client to reboot an instance
 func (c *Client) RebootServer(id int) error {
 
 	if err := c.post("/cloud/server/reboot/"+strconv.Itoa(id), nil, nil); err != nil {
@@ -75,40 +83,7 @@ func (c *Client) RebootServer(id int) error {
 	return nil
 }
 
-func (c *Client) DeleteServer(id int) error {
-
-	if err := c.post("/cloud/server/delete/"+strconv.Itoa(id), nil, nil); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (c *Client) CreateServer(name, plan string, locationID, osID int, options *ServerOptions) (server Server, err error) {
-
-	values := map[string]string{"plan": plan, "fqdn": name, "location": strconv.Itoa(locationID), "image": strconv.Itoa(osID)}
-
-	if options != nil {
-		if options.SSHKeyID != 0 {
-			values["ssh_key_id"] = strconv.Itoa(options.SSHKeyID)
-		}
-		if options.Password != "" {
-			values["password"] = options.Password
-		}
-		if options.CloudConfig != "" {
-			values["cloud_config"] = base64.StdEncoding.EncodeToString([]byte(options.CloudConfig))
-		}
-	}
-
-	postData, _ := json.Marshal(values)
-
-	if err := c.post("/cloud/buy_build/", postData, &server); err != nil {
-		return Server{}, err
-	}
-
-	return server, nil
-}
-
+//ProvisionServer external method on Client to re-build an instance
 func (c *Client) ProvisionServer(name string, id, locationID, osID int, options *ServerOptions) (JobID, error) {
 
 	var jobid JobID
@@ -135,4 +110,52 @@ func (c *Client) ProvisionServer(name string, id, locationID, osID int, options 
 	}
 
 	return jobid, nil
+}
+
+//DeleteServer external method on Client to destroy an instance
+func (c *Client) DeleteServer(id int) error {
+
+	if err := c.post("/cloud/server/delete/"+strconv.Itoa(id), nil, nil); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+//CreateServer external method on Client to buy and build a new instance
+func (c *Client) CreateServer(name, plan string, locationID, osID int, options *ServerOptions) (server Server, err error) {
+
+	values := map[string]string{"plan": plan, "fqdn": name, "location": strconv.Itoa(locationID), "image": strconv.Itoa(osID)}
+
+	if options != nil {
+		if options.SSHKeyID != 0 {
+			values["ssh_key_id"] = strconv.Itoa(options.SSHKeyID)
+		}
+		if options.Password != "" {
+			values["password"] = options.Password
+		}
+		if options.CloudConfig != "" {
+			values["cloud_config"] = base64.StdEncoding.EncodeToString([]byte(options.CloudConfig))
+		}
+	}
+
+	postData, _ := json.Marshal(values)
+
+	if err := c.post("/cloud/buy_build/", postData, &server); err != nil {
+		return Server{}, err
+	}
+
+	return server, nil
+}
+
+//CancelServer external method on Client to cancel/remove from billing an instance
+//this method completely removes an instance, it cannot be rebuilt afterward
+//billing should be prorated to the day or something like that
+func (c *Client) CancelServer(id int) error {
+
+	if err := c.post("/cloud/cancel/"+strconv.Itoa(id), nil, nil); err != nil {
+		return err
+	}
+
+	return nil
 }
